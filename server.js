086224f15dd3bef8a888d704fd33abd76fb6fdf6
Tom Metcalf <intermet@gmail.com>
2025-09-09 22:12:33 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve all static files from the current directory
+app.use(express.static(path.join(__dirname)));
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
